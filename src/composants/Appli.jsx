957216ad33d421913a8643c37b3941b142895bef
Gregory Osorio <e1977078@cmaisonneuve.qc.ca>
2023-02-27 17:03:10 -0500

    Gestion des sauvegardes des dossiers

--- src/composants/Appli.jsx
+++ src/composants/Appli.jsx
@@ -4,15 +4,27 @@ import ListeDossiers from './ListeDossiers';
 import FrmDossier from './FrmDossier';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import { useEffect, useState } from 'react';
 
 export default function Appli() {
+  const[frmDossierOuvert, setFrmDossierOuvert] = useState(false);
+  //État des dossiers de l'utilisateur 
+  const[dossiers, setDossiers] = useState(
+    ()=>JSON.parse(localStorage.getItem('4pa-dossiers')) || []
+  );
+
+  useEffect(
+    ()=>localStorage.setItem('4pa-dossiers', JSON.stringify(dossiers))
+    , [dossiers]
+    );
+
   return (
     <div className="Appli">
         <Entete />
         <section className="contenu-principal">
           <ListeDossiers />
-          <FrmDossier ouvert={false} setOuvert={true}/>
-          <Fab size="large" className="ajoutDossier" color="secondary" aria-label="Ajouter dossier">
+          <FrmDossier ouvert={frmDossierOuvert} setOuvert={setFrmDossierOuvert}/>
+          <Fab onClick={() => setFrmDossierOuvert(true)} size="large" className="ajoutDossier" color="secondary" aria-label="Ajouter dossier">
             <AddIcon />
           </Fab>
         </section>
